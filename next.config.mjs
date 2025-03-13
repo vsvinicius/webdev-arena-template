6cd1a1751f0080d7a2a3df22f5d623f657309597
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2025-03-13 15:28:25 -0300

    bump next patch version and add script to check typescript errors and linting

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
